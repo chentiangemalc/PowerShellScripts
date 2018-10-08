db21f813b7597f4e66fe715b8f72960f5fdffa31
Malcolm McCaffery <malcolm.mccaffery@gmail.com>
2018-10-08 23:45:21 +1100

    Update ConvertEtl-ToPcap.ps1

---
+++ ConvertEtl-ToPcap.ps1
@@ -0,0 +1,106 @@
+﻿Set-StrictMode -Version Latest
+
+$csharp_code = @'
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chentiangemalc
+{
+    public static class NetworkRoutines
+    {
+        public static void ConvertEtlToPcap(string source, string destination, UInt32 maxPacketSize)
+        {
+            var networkTrace = new Guid("{00000001-0000-0000-0000-000000000000}");
+            using (BinaryWriter writer = new BinaryWriter(File.Open(destination, FileMode.Create)))
+            {
+
+                UInt32 magic_number = 0xa1b2c3d4;
+                UInt16 version_major = 2;
+                UInt16 version_minor = 4;
+                Int32 thiszone = 0;
+                UInt32 sigfigs = 0;
+                UInt32 snaplen = maxPacketSize;
+                UInt32 network = 1; // LINKTYPE_ETHERNET
+
+                writer.Write(magic_number);
+                writer.Write(version_major);
+                writer.Write(version_minor);
+                writer.Write(thiszone);
+                writer.Write(sigfigs);
+                writer.Write(snaplen);
+                writer.Write(network);
+
+                using (var reader = new EventLogReader(source, PathType.FilePath))
+                {
+                    EventRecord record;
+                    while ((record = reader.ReadEvent()) != null)
+                    {
+                        using (record)
+                        {
+                            if (record.ActivityId == networkTrace)
+                            {
+                                DateTime timeCreated = (DateTime)record.TimeCreated;
+
+                                UInt32 ts_sec = (UInt32)((timeCreated.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+                                UInt32 ts_usec = (UInt32)(((timeCreated.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds) - ((UInt32)((timeCreated.Subtract(new DateTime(1970, 1, 1))).TotalSeconds * 1000))) * 1000;
+                                UInt32 incl_len = (UInt32)record.Properties[2].Value;
+                                if (incl_len > maxPacketSize)
+                                {
+                                    throw new System.InvalidOperationException(String.Format("Packet size of {0} exceeded max packet size {1}", incl_len, maxPacketSize));
+                                }
+                                UInt32 orig_len = incl_len;
+
+                                writer.Write(ts_sec);
+                                writer.Write(ts_usec);
+                                writer.Write(incl_len);
+                                writer.Write(orig_len);
+                                writer.Write((byte[])record.Properties[3].Value);
+
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
+'@
+
+Add-Type -Type $csharp_code
+
+Function ConvertEtl-ToPcap
+{
+    param(
+        [ValidateScript({
+            if( -Not ($_ | Test-Path) ){
+                throw "File or folder $_ does not exist"
+            }
+
+            if($_.Extension -ne ".etl"){
+                throw "Source file must be .etl file"
+            }
+            return $true
+        })]
+        [System.IO.FileInfo]$Path,
+        [ValidateScript({
+            if( -Not ($path.DirectoryName | Test-Path) ){
+                throw "File or folder does not exist"
+            }
+
+            if($_.Extension -ne ".pcap") {
+                throw "Estination file must be .pcap file"
+            }
+            return $true
+        })]
+        [System.IO.FileInfo]$Destination,
+        [Uint32]$MaxPacketSizeBytes = 65536)
+
+   [chentiangemalc.NetworkRoutines]::ConvertEtlToPcap($PAth.FullName,$Destination.FullName,$MaxPacketSizeBytes)
+}
